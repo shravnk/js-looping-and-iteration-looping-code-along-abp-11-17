03909f22f3449c6158b5d28153914b0d66d132b9
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-11 05:38:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i=0;i < names.length;i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
+  }
+  return names
+}
+function tailsNeverFails() {
+  let i = -1
+  let test_case = 1
+  while (test_case >= 0.5) {
+    test_case = Math.random()
+    i++
+  }
+
+const result = "You got " + i + " tails in a row!"
+return result
+}
